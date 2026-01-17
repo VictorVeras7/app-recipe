b1380f76227b1b9b757ceafa7dddaee81bef606c
Victor Veras <victorveras766@gmail.com>
2026-01-17 18:53:21 -0300

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import { ENV } from './config/env.js';
+import { db } from './config/db.js';
+import { favoritesTable } from './db/schema.js';
+
+const app = express();
+const PORT = ENV.PORT;
+
+app.use(express.json());
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ sucess: true, message: "API is healthy" });
+});
+
+app.post("/api/favorites", async (req, res) => {
+  try {
+    const { userId, recipeId, title, image, cookTime, servings } = req.body;  
+
+    if (!userId || !recipeId || !title) {
+      return res.status(400).json({ success: false, message: "Missing required fields" });
+    }
+
+    const newFavorite = await db.insert(favoritesTable).values({
+      userId,
+      recipeId,
+      title,
+      image,
+      cookTime,
+      servings
+    })
+    .returning();
+    res.status(201).json(newFavorite[0]);
+
+  } catch (error) {
+    console.error("Error adding favorite:", error);
+    res.status(500).json({ success: false, message: "Failed to add favorite" });
+  }
+
+
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
